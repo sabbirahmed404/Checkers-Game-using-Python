83b3645283e7061bd12d83aaa87e8bd2aa45c5a2
RaihanKabir277 <raihank0198@gmail.com>
2025-04-15 17:44:26 +0600

    main file updated with related game and minimax file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pygame
 
 from checkers.constants import width, height, square_size, red, white
+from checkers.game import Game
+from minimax.algorithm import minimax
 
 
 fps = 60
@@ -18,8 +20,33 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
+    game = Game(win)
+
+    while run:
+        clock.tick(fps)
+
+        if game.turn == white:
+            value, new_board = minimax(game.get_board(), 4, white, game)
+            game.ai_move(new_board)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+
+    pygame.quit()
+
     
    
-
-
+   
 main()
