b0f562b2ff86c5631aa8bf8c9f5839e19a74c487
RaihanKabir277 <raihank0198@gmail.com>
2025-04-15 11:32:33 +0600

    constants added for ui design of project

---
+++ checkers/constants.py
@@ -0,0 +1,15 @@
+
+import pygame
+
+width, height = 800, 800
+rows, cols = 8, 8
+square_size = width / cols
+
+
+red = (255, 0, 0)
+white = (255, 255, 255)
+black= (255, 255, 255)
+blue = (0, 0, 255)
+grey = (128,128,128)
+
+
